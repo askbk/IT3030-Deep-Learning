708c73d0ef5260e8a0bc601841ea9d514a089749
askbk <askbk@protonmail.com>
2021-01-19 09:15:46 +0100

    Add constructor tests.

--- Backpropagation/ImageGenerator.py
+++ Backpropagation/ImageGenerator.py
@@ -3,4 +3,13 @@ class ImageGenerator:
     Class for generating images.
     """
 
-    pass
+    def __init__(
+        self,
+        side_length=10,
+        noise=0,
+        image_set_size=100,
+        image_set_fractions=(0.7, 0.2, 0.1),
+        figure_size_range=(7, 10),
+        flatten=False,
+    ):
+        pass
--- Backpropagation/ImageGenerator_test.py
+++ Backpropagation/ImageGenerator_test.py
@@ -1,4 +1,13 @@
 from ImageGenerator import ImageGenerator
 
+
 def test_image_generator_constructor():
     ImageGenerator()
+    ImageGenerator(
+        side_length=10,
+        noise=0.1,
+        image_set_fractions=(0.7, 0.2, 0.1),
+        figure_size_range=(7, 10),
+        flatten=False,
+        image_set_size=100,
+    )
