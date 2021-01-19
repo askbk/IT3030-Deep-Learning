c65aa87924a0d2a6c2870a3fb4c454612b786333
askbk <askbk@protonmail.com>
2021-01-19 09:54:07 +0100

    Add flattening option.

--- Backpropagation/ImageGenerator.py
+++ Backpropagation/ImageGenerator.py
@@ -1,3 +1,6 @@
+from itertools import chain
+
+
 class ImageGenerator:
     """
     Class for generating images.
@@ -13,14 +16,34 @@ class ImageGenerator:
         pass
 
     def generate(
-        self, image_set_size=100, image_set_fractions=(0.7, 0.2, 0.1), side_length=10
+        self,
+        image_set_size=100,
+        image_set_fractions=(0.7, 0.2, 0.1),
+        side_length=10,
+        flatten=False,
     ):
         """
         Generates images.
         """
         training, validation, test = image_set_fractions
+
+        image_set = [[[0] * side_length] * side_length] * image_set_size
+        training_index = int(training * image_set_size)
+        validation_index = int((training + validation) * image_set_size)
+
+        training_set = image_set[:training_index]
+        validation_set = image_set[training_index : validation_index + 1]
+        test_set = image_set[validation_index + 1 :]
+
+        if not flatten:
+            return (
+                training_set,
+                validation_set,
+                test_set,
+            )
+
         return (
-            [[[0] * side_length] * side_length] * int(image_set_size * training),
-            [[[0] * side_length] * side_length] * int(image_set_size * validation),
-            [[[0] * side_length] * side_length] * int(image_set_size * test),
+            list(map(lambda image: list(chain.from_iterable(image)), training_set)),
+            list(map(lambda image: list(chain.from_iterable(image)), validation_set)),
+            list(map(lambda image: list(chain.from_iterable(image)), test_set)),
         )
--- Backpropagation/ImageGenerator_test.py
+++ Backpropagation/ImageGenerator_test.py
@@ -18,7 +18,7 @@ def test_generates_correct_number_of_images():
 
 def test_generates_correct_set_fractions():
     training, validation, test = ImageGenerator().generate(
-        image_set_fractions=(0.7, 0.2, 0.1)
+        image_set_fractions=(0.7, 0.2, 0.1), image_set_size=400
     )
 
     total = len(training) + len(validation) + len(test)
@@ -30,11 +30,27 @@ def test_generates_correct_set_fractions():
 
 def test_correct_side_length():
     side_length = 10
-    training, validation, test = ImageGenerator().generate(side_length=side_length)
+    training, validation, test = ImageGenerator().generate(
+        side_length=side_length, flatten=False
+    )
 
     assert all(
         map(
             lambda image: len(image) == side_length and len(image[0]) == side_length,
             [*training, *validation, *test],
+        )
+    )
+
+
+def test_flatten():
+    side_length = 15
+    training, validation, test = ImageGenerator().generate(
+        flatten=True, side_length=side_length
+    )
+    print(test)
+    assert all(
+        map(
+            lambda image: len(image) == side_length * side_length,
+            [*training, *test, *validation],
         )
     )
