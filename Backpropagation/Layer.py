7fda21e681825de21b00252374c584cac0965ef6
askbk <askbk@protonmail.com>
2021-01-19 19:57:15 +0100

    Temp commit.

--- Backpropagation/Layer.py
+++ Backpropagation/Layer.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Layer:
     """
     A layer in a neural network.
@@ -5,9 +8,38 @@ class Layer:
 
     def __init__(
         self,
+        input_neurons=5,
         neurons=5,
         activation_function="sigmoid",
         softmax=False,
         initial_weight_range=(-0.1, 0.1),
+        weights=None,
     ):
-        pass
+        self._neurons = neurons
+        if weights is not None:
+            self._weights = weights
+        else:
+            self._weights = np.random.uniform(
+                low=initial_weight_range[0],
+                high=initial_weight_range[1],
+                size=(neurons, input_neurons),
+            )
+
+    @staticmethod
+    def _sigmoid(X):
+        """
+        Sigmoid function
+        """
+        return 1 / (1 + np.exp(-X))
+
+    def _multiply_weights_input(self, X: np.array):
+        """
+        docstring
+        """
+        return X @ self._weights
+
+    def forward_pass(self, data):
+        """
+        Data is a minibatch of data.
+        """
+        return Layer._sigmoid(np.sum(self._multiply_weights_input(data)))
--- Backpropagation/Layer_test.py
+++ Backpropagation/Layer_test.py
@@ -1,4 +1,5 @@
 from Layer import Layer
+import numpy as np
 
 
 def test_layer_constructor():
@@ -8,4 +9,38 @@ def test_layer_constructor():
         activation_function="sigmoid",
         softmax=False,
         initial_weight_range=(-0.1, 0.1),
+    )
+
+
+def test_forward_pass():
+    layer = Layer(neurons=1, input_neurons=1)
+    data = np.array([[0], [1]])
+
+    output = layer.forward_pass(data)
+    assert not any(np.isnan(output))
+
+
+def test_forward_pass_correct_output():
+    layer = Layer(
+        input_neurons=1,
+        weights=np.array([[1]]),
+        activation_function="sigmoid",
+        softmax=False,
+    )
+    actual = layer.forward_pass(np.array([[-1], [0], [1]]))
+    expected = [[0.26894142], [0.5], [0.73105858]]
+    assert all([np.isclose(a, b) for a, b in zip(actual, expected)])
+
+    layer = Layer(
+        input_neurons=2,
+        neurons=1,
+        weights=np.array([[0.1, 0.2]]),
+        activation_function="sigmoid",
+        softmax=False,
+    )
+    actual = layer.forward_pass(np.array([[-1, 1], [0, -1], [1, 0]]))
+    expected = [[0.52498], [0.45017], [0.52498]]
+    print(actual)
+    assert all(
+        [np.isclose(a, b) for x, y in zip(actual, expected) for a, b in zip(x, y)]
     )
