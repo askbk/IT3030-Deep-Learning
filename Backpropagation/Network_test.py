a8607de35de994838ff6bdd82844de10a5365aaf
askbk <askbk@protonmail.com>
2021-01-21 17:31:17 +0100

    Add test with input layer.

--- Backpropagation/Network_test.py
+++ Backpropagation/Network_test.py
@@ -1,5 +1,6 @@
 from Network import Network
 from Layer import Layer
+from InputLayer import InputLayer
 import numpy as np
 
 
@@ -64,5 +65,30 @@ def test_network_forward_pass_multiple_layers():
     expected = np.array(
         [[0.57384083, 0.55911531], [0.57566333, 0.56156158], [0.57748676, 0.56401704]]
     )
+
+    assert np.all(np.isclose(actual, expected))
+
+
+def test_network_with_input_layer():
+    network = Network(
+        layers=[
+            InputLayer(),
+            Layer(
+                input_neurons=2,
+                neurons=1,
+                activation_function="sigmoid",
+                softmax=False,
+                weights=np.array([[0.1], [-0.3]]),
+                bias=True,
+                bias_weights=np.array([[0.2]]),
+            ),
+        ],
+        loss_function="mse",
+        regularization=None,
+    )
+
+    data = np.array([[1, 1], [0, 0], [-1, 1]])
+    actual = network.forward_pass(data)
+    expected = np.array([[0.5], [0.549834], [0.450166]])
 
     assert np.all(np.isclose(actual, expected))
