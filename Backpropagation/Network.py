12fbbf23ada9447d280308a23681e21e3cb9af0c
askbk <askbk@protonmail.com>
2021-01-21 20:05:33 +0100

    Some more work on training.

--- Backpropagation/Network.py
+++ Backpropagation/Network.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import numpy as np
 
 
 class Network:
@@ -8,6 +9,7 @@ class Network:
 
     def __init__(self, layers, loss_function, regularization, regularization_rate=None):
         self._layers = layers
+        self._loss_function = loss_function
 
     def forward_pass(self, X):
         """
@@ -15,7 +17,26 @@ class Network:
         """
         return reduce(lambda data, layer: layer.forward_pass(data), self._layers, X)
 
+    @staticmethod
+    def _mean_squared_error(Y: np.array, Y_hat: np.array):
+        """
+        Calculates mean squared error.
+        """
+        return ((Y - Y_hat) ** 2).mean(axis=1)
+
+    def _apply_loss_function(self, Y, Y_hat):
+        """
+        docstring
+        """
+        if self._loss_function == "mse":
+            return Network._mean_squared_error(Y, Y_hat)
+
+        raise NotImplementedError
+
     def _train_minibatch(self, batch):
+        X, Y = batch
+        output = self.forward_pass(X)
+        loss = self._apply_loss_function(Y, output)
         return self
 
     def train(self, X, Y, minibatches=1):
