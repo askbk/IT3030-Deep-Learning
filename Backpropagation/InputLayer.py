6ae0f43804c3201fdc9f11694f6baddff91f0320
askbk <askbk@protonmail.com>
2021-01-21 17:19:21 +0100

    Add InputLayer class.

---
+++ Backpropagation/InputLayer.py
@@ -0,0 +1,10 @@
+class InputLayer:
+    """
+    Represents an input layer with no weights or biases.
+    """
+
+    def forward_pass(self, data):
+        """
+        Passes the data through the layer without modifying it.
+        """
+        return data
