7a26624952c91e1e326486498c8fc72af531504b
askbk <askbk@protonmail.com>
2021-01-21 11:00:56 +0100

    Add forward_pass method.

--- Backpropagation/Network.py
+++ Backpropagation/Network.py
@@ -3,5 +3,11 @@ class Network:
     A neural network.
     """
 
-    def __init__(self, layers, loss_function, regularization, regularization_rate):
-        pass
+    def __init__(self, layers, loss_function, regularization, regularization_rate=None):
+        self._layers = layers
+
+    def forward_pass(self, X):
+        """
+        Forward pass through the network.
+        """
+        return self._layers[0].forward_pass(X)
--- Backpropagation/Network_test.py
+++ Backpropagation/Network_test.py
@@ -1,7 +1,32 @@
 from Network import Network
+from Layer import Layer
+import numpy as np
 
 
 def test_network_constructor():
     Network(
         layers=[], loss_function="mse", regularization=None, regularization_rate=0.001
     )
+
+
+def test_network_forward_pass_base_case():
+    network = Network(
+        layers=[
+            Layer(
+                input_neurons=1,
+                neurons=1,
+                activation_function="sigmoid",
+                softmax=False,
+                weights=[[1]],
+                bias=True,
+                bias_weights=[1],
+            )
+        ],
+        loss_function="mse",
+        regularization=None,
+    )
+
+    actual = network.forward_pass(np.array([[-1], [0], [1]]))
+    expected = np.array([[0.5], [0.73105858], [0.8807971]])
+
+    assert np.all(np.isclose(actual, expected))
