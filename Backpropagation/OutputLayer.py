e2967d6293816e15f9dd4b481dfc1c9506b27837
askbk <askbk@protonmail.com>
2021-01-26 09:20:50 +0100

    Add input_neurons parameter to OutputLayer

--- Backpropagation/OutputLayer.py
+++ Backpropagation/OutputLayer.py
@@ -6,8 +6,9 @@ class OutputLayer:
     An output layer.
     """
 
-    def __init__(self, softmax=False):
+    def __init__(self, input_neurons, softmax=False):
         self._use_softmax = softmax
+        self._input_neurons = input_neurons
 
     @staticmethod
     def _softmax(X):
@@ -15,6 +16,13 @@ class OutputLayer:
         The softmax function.
         """
         return np.exp(X) / np.sum(np.exp(X), axis=1, keepdims=True)
+
+    def backward_pass(self, loss):
+        """
+        Computes Jacobian.
+        """
+        if self._use_softmax:
+            raise NotImplementedError("Softmax Jacobian not implemented.")
 
     def forward_pass(self, data):
         if self._use_softmax:
--- Backpropagation/OutputLayer_test.py
+++ Backpropagation/OutputLayer_test.py
@@ -4,14 +4,14 @@ import numpy as np
 
 def test_returns_input_without_softmax():
     data = np.array([[1, 2, 3], [2, 3, 4]])
-    output = OutputLayer().forward_pass(data)
+    output = OutputLayer(input_neurons=3).forward_pass(data)
 
     assert np.all(np.isclose(output, data))
 
 
 def test_applies_softmax():
     data = np.array([[1, 2, 3], [0.1, 0.3, 0.9]])
-    output = OutputLayer(softmax=True).forward_pass(data)
+    output = OutputLayer(input_neurons=3, softmax=True).forward_pass(data)
     expected = np.array(
         [[0.09003057, 0.24472847, 0.66524096], [0.22487355, 0.27466117, 0.50046528]]
     )
