51f544f113a78e2f061fa160359de38473615bc4
askbk <askbk@protonmail.com>
2021-02-02 14:49:11 +0100

    Add function for translating labels to neurons.

--- Backpropagation/DataUtils.py
+++ Backpropagation/DataUtils.py
@@ -1,4 +1,6 @@
 import random
+import numpy as np
+from functools import reduce
 
 
 def randomize_dataset(dataset):
@@ -6,3 +8,16 @@ def randomize_dataset(dataset):
     Randomises order of dataset
     """
     return random.sample(dataset, k=len(dataset))
+
+
+def translate_labels_to_neuron_activation(dataset):
+    labels = reduce(lambda labels_set, case: labels_set | {case[1]}, dataset, set())
+    neuron_dict = {label: i for i, label in enumerate(labels)}
+    neuron_count = len(labels)
+
+    def translated(label):
+        return np.array(
+            [1 if neuron_dict[label] == x else 0 for x in range(neuron_count)]
+        )
+
+    return list(map(lambda case: (case[0], translated(case[1])), dataset))
