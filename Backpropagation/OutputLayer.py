Implement softmax backward pass and fix tests

Backpropagation/OutputLayer.py
@@ -17,23 +17,31 @@ class OutputLayer:
         """
         return np.exp(X) / np.sum(np.exp(X), axis=1, keepdims=True)
 
-    def backward_pass(self, J_L_Z, Z, Y):
+    @staticmethod
+    def _softmax_derivative(X):
+        X_reshape = X.reshape((-1, 1))
+        return np.diagflat(X) - np.dot(X_reshape, X_reshape.T)
+
+    def backward_pass(self, J_L_S, S, Z):
         """
         Computes Jacobian.
         """
-        if self._use_softmax:
-            raise NotImplementedError
+        if not self._use_softmax:
+            return None, J_L_S
+
+        J_S_Z = OutputLayer._softmax_derivative(Z)
+        J_L_Z = np.dot(J_L_S, J_S_Z)
 
         return None, J_L_Z
 
     def forward_pass(self, data):
-        if self._use_softmax:
-            return OutputLayer._softmax(data)
+        if not self._use_softmax:
+            return data
 
-        return data
+        return OutputLayer._softmax(data)
 
     def update_weights(self, jacobians, learning_rate):
-        if self._use_softmax:
-            raise NotImplementedError
+        if not self._use_softmax:
+            return self
 
         return self

Backpropagation/OutputLayer_test.py
@@ -15,10 +15,13 @@ def test_applies_softmax():
     expected = np.array(
         [[0.09003057, 0.24472847, 0.66524096], [0.22487355, 0.27466117, 0.50046528]]
     )
+    print(output)
     assert np.all(np.isclose(output, expected))
 
 
-# def test_backward_pass_no_softmax():
-#     data = np.array([[1, 2, 3, 6, 4], [1, 2, 6, 3, 5]])
-#     output = OutputLayer(input_neurons=5, softmax=False).backward_pass()
-#     # assert np.all(np.isclose(output, np.diag(np.ones(5))))
+def test_backward_pass_no_softmax():
+    data = np.array([[1, 2, 3, 6, 4], [1, 2, 6, 3, 5]])
+    output = OutputLayer(input_neurons=5, softmax=False).backward_pass(
+        data, None, None
+    )[1]
+    assert np.all(np.isclose(output, data))