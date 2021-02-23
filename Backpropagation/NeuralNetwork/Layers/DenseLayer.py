Remove swish and fix derivatives

Backpropagation/NeuralNetwork/Layers/DenseLayer.py
@@ -21,7 +21,7 @@ class DenseLayer(LayerBase):
             weights, input_neurons, neurons, initial_weight_range, use_bias
         )
 
-        if activation_function not in ("sigmoid", "tanh", "relu", "linear", "swish"):
+        if activation_function not in ("sigmoid", "tanh", "relu", "linear"):
             raise ValueError("Invalid activation function.")
         self._use_bias = use_bias
         self._neurons = neurons
@@ -69,9 +69,6 @@ class DenseLayer(LayerBase):
         if self._activation_function == "linear":
             return Activation.linear(data)
 
-        if self._activation_function == "swish":
-            return Activation.swish(data)
-
         raise NotImplementedError(
             f"Activation function {self._activation_function} not implemented."
         )
@@ -91,9 +88,6 @@ class DenseLayer(LayerBase):
 
         if self._activation_function == "linear":
             return Activation.linear_derivative(data)
-
-        if self._activation_function == "swish":
-            return Activation.swish_derivative(data)
 
         raise NotImplementedError(
             f"Activation function {self._activation_function} not implemented."
@@ -128,7 +122,6 @@ class DenseLayer(LayerBase):
         J_Z_Sum = np.diag(Diag_J_Z_Sum)
         # Don't include bias node in Jacobian passed upstream
         J_Z_Y = np.dot(J_Z_Sum, self._get_weights_excluding_bias().T)
-        # J_hat_Z_W = np.outer(Diag_J_Z_Sum, self._add_bias_neuron_conditionally(Y))
         J_hat_Z_W = np.outer(self._add_bias_neuron_conditionally(Y), Diag_J_Z_Sum)
         J_L_W = J_L_Z * J_hat_Z_W
         J_L_Y = np.dot(J_L_Z, J_Z_Y)

Backpropagation/NeuralNetwork/Math.py
@@ -11,16 +11,8 @@ class Activation:
         return expit(X)
 
     @staticmethod
-    def sigmoid_derivative(X):
-        return Activation.sigmoid(X) * (1 - Activation.sigmoid(X))
-
-    @staticmethod
-    def swish(X):
-        return X * Activation.sigmoid(X)
-
-    @staticmethod
-    def swish_derivative(X):
-        return Activation.swish(X) + Activation.sigmoid(X) * (1 - Activation.swish(X))
+    def sigmoid_derivative(Y):
+        return Y * (1.0 - Y)
 
     @staticmethod
     def tanh(X):
@@ -30,11 +22,11 @@ class Activation:
         return np.tanh(X)
 
     @staticmethod
-    def tanh_derivative(X):
+    def tanh_derivative(Y):
         """
         Hyperbolic tangent
         """
-        return 1 - Activation.tanh(X) ** 2
+        return 1 - Y ** 2
 
     @staticmethod
     def linear(X):
@@ -44,8 +36,8 @@ class Activation:
         return X
 
     @staticmethod
-    def linear_derivative(X):
-        return np.ones_like(X)
+    def linear_derivative(Y):
+        return np.ones_like(Y)
 
     @staticmethod
     def relu(X):
@@ -55,8 +47,8 @@ class Activation:
         return np.maximum(X, 0)
 
     @staticmethod
-    def relu_derivative(X):
-        return np.where(X <= 0, 0, 1)
+    def relu_derivative(Y):
+        return np.where(Y <= 0, 0, 1)
 
     @staticmethod
     def softmax(X):
@@ -66,9 +58,10 @@ class Activation:
         return softmax(X, axis=0)
 
     @staticmethod
-    def softmax_derivative(X):
-        X_reshape = X.reshape((-1, 1))
-        return np.diagflat(X) - np.dot(X_reshape, X_reshape.T)
+    def softmax_derivative(S):
+        S_vector = S.reshape(S.shape[0], 1)
+        S_matrix = np.tile(S_vector, S.shape[0])
+        return np.diag(S) - (S_matrix * np.transpose(S_matrix))
 
 
 class Loss: