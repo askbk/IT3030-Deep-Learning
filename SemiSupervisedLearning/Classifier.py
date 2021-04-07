7047e6cb0e141b382979b807f13672a7b9c28f2e
askbk <askbk@protonmail.com>
2021-04-07 16:20:49 +0200

    Refactor classifier

--- SemiSupervisedLearning/Classifier.py
+++ SemiSupervisedLearning/Classifier.py
@@ -5,13 +5,22 @@ import tensorflow.keras as keras
 from Autoencoder import Encoder
 
 
-class Classifier(keras.Model):
-    def __init__(self, encoder=None):
-        super(Classifier, self).__init__()
-        self._encoder = Encoder() if encoder is None else encoder
+class ClassifierHead(keras.Model):
+    def __init__(self):
+        super(ClassifierHead, self).__init__()
         self._layers = [keras.layers.Dense(10, activation="softmax")]
 
     def call(self, inputs):
-        return reduce(
-            lambda data, layer: layer(data), self._layers, self._encoder(inputs)
+        return reduce(lambda data, layer: layer(data), self._layers, inputs)
+
+
+class Classifier(keras.Model):
+    def __init__(self, encoder=None, classifier_head=None):
+        super(Classifier, self).__init__()
+        self._encoder = Encoder() if encoder is None else encoder
+        self._classifier_head = (
+            ClassifierHead() if classifier_head is None else classifier_head
         )
+
+    def call(self, inputs):
+        return self._classifier_head(self._encoder(inputs))
