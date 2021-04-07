12eebbb4b86b42a970af6424e97a71bdce8e0c96
askbk <askbk@protonmail.com>
2021-04-07 20:58:09 +0200

    Refactor

--- SemiSupervisedLearning/test_learners.py
+++ SemiSupervisedLearning/test_learners.py
@@ -13,30 +13,23 @@ def load_dataset(dataset_name: str):
 
 
 def get_preprocessed_data(
-    dataset: str, semi_supervised_split=False, split_ratio: float = None
+    dataset: str, semi_supervised_split=False, labeled_fraction: float = None
 ):
-    train, test = load_dataset(dataset)
+    (x_train, y_train), (x_test, y_test) = load_dataset(dataset)
+    x_train = preprocess(x_train)
+    x_test = preprocess(x_test)
+    y_train = keras.utils.to_categorical(y_train)
+    y_test = keras.utils.to_categorical(y_test)
 
     if not semi_supervised_split:
-        return (preprocess(train[0]), keras.utils.to_categorical(train[1])), (
-            preprocess(test[0]),
-            keras.utils.to_categorical(test[1]),
-        )
+        return (x_train, y_train), (x_test, y_test)
 
     (x_train_unlabeled, _), (x_train_labeled, y_train_labeled) = semisupervised_split(
-        train, split_ratio
+        (x_train, y_train), labeled_fraction
     )
     (x_test_unlabeled, _), (x_test_labeled, y_test_labeled) = semisupervised_split(
-        test, split_ratio
+        (x_test, y_test), labeled_fraction
     )
-
-    x_train_unlabeled = preprocess(x_train_unlabeled)
-    x_test_unlabeled = preprocess(x_test_unlabeled)
-    x_train_labeled = preprocess(x_train_labeled)
-    x_test_labeled = preprocess(x_test_labeled)
-
-    y_train_labeled = keras.utils.to_categorical(y_train_labeled)
-    y_test_labeled = keras.utils.to_categorical(y_test_labeled)
 
     return (x_train_unlabeled, x_train_labeled, y_train_labeled), (
         x_test_unlabeled,
@@ -84,7 +77,7 @@ def test_supervised_classifier():
 
 def test_semi_supervised_classifier():
     (x1_train, x2_train, y2_train), (x1_test, x2_test, y2_test) = get_preprocessed_data(
-        "mnist", semi_supervised_split=True, split_ratio=0.5
+        "mnist", semi_supervised_split=True, labeled_fraction=0.5
     )
     autoencoder = Autoencoder()
     autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
@@ -115,4 +108,4 @@ def test_semi_supervised_classifier():
 
 
 if __name__ == "__main__":
-    test_supervised_classifier()
+    test_semi_supervised_classifier()
