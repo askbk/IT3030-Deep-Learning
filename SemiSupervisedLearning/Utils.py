d4887eced015a145d278c046a1e6c304d8c54937
askbk <askbk@protonmail.com>
2021-04-07 20:35:54 +0200

    Change splitter

--- SemiSupervisedLearning/Utils.py
+++ SemiSupervisedLearning/Utils.py
@@ -5,10 +5,7 @@ import matplotlib.pyplot as plt
 def semisupervised_split(dataset, labeled_fraction=0.1):
     x, y = dataset
     split_index = int(len(x) * (1 - labeled_fraction))
-    return (np.expand_dims(x[:split_index], -1), np.expand_dims(y[:split_index], -1)), (
-        np.expand_dims(x[split_index:], -1),
-        np.expand_dims(y[split_index:], -1),
-    )
+    return (x[:split_index], y[:split_index]), (x[split_index:], y[split_index:])
 
 
 def preprocess(array):
