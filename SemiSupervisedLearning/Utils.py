f90abcfd5d0cd275585b9694318ad640b4b4dbdf
askbk <askbk@protonmail.com>
2021-04-08 19:44:59 +0200

    Move helper functions

--- SemiSupervisedLearning/Utils.py
+++ SemiSupervisedLearning/Utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow import keras
+from tensorflow.keras import datasets
 
 
 def semisupervised_split(dataset, labeled_fraction=0.1):
@@ -44,3 +46,34 @@ def display(array1, array2):
         ax.get_yaxis().set_visible(False)
 
     plt.show()
+
+
+def load_dataset(dataset_name: str):
+    if dataset_name == "mnist":
+        return datasets.mnist.load_data()
+
+
+def get_preprocessed_data(
+    dataset: str, semi_supervised_split=False, labeled_fraction: float = None
+):
+    (x_train, y_train), (x_test, y_test) = load_dataset(dataset)
+    x_train = preprocess(x_train)
+    x_test = preprocess(x_test)
+    y_train = keras.utils.to_categorical(y_train)
+    y_test = keras.utils.to_categorical(y_test)
+
+    if not semi_supervised_split:
+        return (x_train, y_train), (x_test, y_test)
+
+    (x_train_unlabeled, _), (x_train_labeled, y_train_labeled) = semisupervised_split(
+        (x_train, y_train), labeled_fraction
+    )
+    (x_test_unlabeled, _), (x_test_labeled, y_test_labeled) = semisupervised_split(
+        (x_test, y_test), labeled_fraction
+    )
+
+    return (x_train_unlabeled, x_train_labeled, y_train_labeled), (
+        x_test_unlabeled,
+        x_test_labeled,
+        y_test_labeled,
+    )
--- SemiSupervisedLearning/test_learners.py
+++ SemiSupervisedLearning/test_learners.py
@@ -4,38 +4,7 @@ from tensorflow.keras import datasets
 
 from Autoencoder import Autoencoder
 from Classifier import Classifier
-from Utils import semisupervised_split, preprocess, display
-
-
-def load_dataset(dataset_name: str):
-    if dataset_name == "mnist":
-        return datasets.mnist.load_data()
-
-
-def get_preprocessed_data(
-    dataset: str, semi_supervised_split=False, labeled_fraction: float = None
-):
-    (x_train, y_train), (x_test, y_test) = load_dataset(dataset)
-    x_train = preprocess(x_train)
-    x_test = preprocess(x_test)
-    y_train = keras.utils.to_categorical(y_train)
-    y_test = keras.utils.to_categorical(y_test)
-
-    if not semi_supervised_split:
-        return (x_train, y_train), (x_test, y_test)
-
-    (x_train_unlabeled, _), (x_train_labeled, y_train_labeled) = semisupervised_split(
-        (x_train, y_train), labeled_fraction
-    )
-    (x_test_unlabeled, _), (x_test_labeled, y_test_labeled) = semisupervised_split(
-        (x_test, y_test), labeled_fraction
-    )
-
-    return (x_train_unlabeled, x_train_labeled, y_train_labeled), (
-        x_test_unlabeled,
-        x_test_labeled,
-        y_test_labeled,
-    )
+from Utils import semisupervised_split, preprocess, display, get_preprocessed_data
 
 
 def test_autoencoder():
